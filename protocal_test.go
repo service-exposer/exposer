8b64ee02d9299773cdf597569bf7723569bb214b
mofon <immofon@163.com>
2017-02-09 10:39:38 +0800

    add test case on Protocal.Reply

--- protocal_test.go
+++ protocal_test.go
@@ -1,27 +1,42 @@
 package exposer
 
 import (
-	"io"
+	"encoding/json"
 	"net"
-	"os"
+	"reflect"
 	"testing"
 )
 
 func TestProtocal_Reply(t *testing.T) {
-	s, c := net.Pipe()
-	proto := NewProtocal(c)
-	if proto.isHandshakeDone != false {
-		t.Fatal(proto.isHandshakeDone)
-	}
+	{
+		s, c := net.Pipe()
+		proto := NewProtocal(c)
+		if proto.isHandshakeDone != false {
+			t.Fatal(proto.isHandshakeDone)
+		}
 
-	go func() {
-		defer proto.conn.Close()
-		err := proto.Reply("test", nil)
+		go func() {
+			defer proto.conn.Close()
+			err := proto.Reply("test", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}()
+
+		var reply HandshakeIncoming
+		err := json.NewDecoder(s).Decode(&reply)
 		if err != nil {
 			t.Fatal(err)
 		}
-	}()
 
-	io.Copy(os.Stdout, s)
+		var replyExpect = HandshakeIncoming{
+			Command: "test",
+			Details: json.RawMessage([]byte("null")),
+		}
+
+		if !reflect.DeepEqual(&replyExpect, &reply) {
+			t.Fatal("expect", replyExpect, "got", reply)
+		}
+	}
 
 }
