a8eab5fcba9b4cc6acc6e2f5238742ede664c672
mofon <immofon@163.com>
2017-02-20 09:25:03 +0800

    add protocal route

---
+++ protocal/route/route.go
@@ -0,0 +1,118 @@
+package route
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/service-exposer/exposer"
+	"github.com/service-exposer/exposer/protocal/expose"
+	"github.com/service-exposer/exposer/protocal/keepalive"
+	"github.com/service-exposer/exposer/protocal/link"
+	"github.com/service-exposer/exposer/service"
+)
+
+const (
+	CMD_ROUTE       = "route"
+	CMD_ROUTE_REPLY = "route:reply"
+)
+
+type Type string
+
+const (
+	KeepAlive      = "keepalive"
+	Expose    Type = "expose"
+	Link      Type = "link"
+)
+
+type Reply struct {
+	OK  bool
+	Err string
+}
+
+type RouteReq struct {
+	Type Type
+}
+
+func ServerSide() exposer.HandshakeHandleFunc {
+	router := service.NewRouter()
+
+	keepaliveFn := keepalive.ServerSide(0)
+	exposeFn := expose.ServerSide(router)
+	linkFn := link.ServerSide(router)
+
+	return func(proto *exposer.Protocal, cmd string, details []byte) error {
+		switch cmd {
+		case CMD_ROUTE:
+			var req RouteReq
+			err := json.Unmarshal(details, &req)
+			if err != nil {
+				return err
+			}
+
+			switch req.Type {
+			case KeepAlive:
+				err := proto.Reply(CMD_ROUTE_REPLY, &Reply{
+					OK: true,
+				})
+				if err != nil {
+					return err
+				}
+
+				proto.On = keepaliveFn
+			case Expose:
+				err := proto.Reply(CMD_ROUTE_REPLY, &Reply{
+					OK: true,
+				})
+				if err != nil {
+					return err
+				}
+
+				proto.On = exposeFn
+			case Link:
+				err := proto.Reply(CMD_ROUTE_REPLY, &Reply{
+					OK: true,
+				})
+				if err != nil {
+					return err
+				}
+
+				proto.On = linkFn
+			default:
+				err := errors.New(fmt.Sprint("unsupport RouteReq.Type", req.Type))
+				proto.Reply(CMD_ROUTE_REPLY, &Reply{
+					OK:  false,
+					Err: err.Error(),
+				})
+
+				return err
+			}
+
+			return nil
+		}
+
+		return errors.New("unknow cmd: " + cmd)
+	}
+}
+
+func ClientSide(nextHandleFunc exposer.HandshakeHandleFunc, nextCmd string, nextDetails interface{}) exposer.HandshakeHandleFunc {
+	return func(proto *exposer.Protocal, cmd string, details []byte) error {
+		switch cmd {
+		case CMD_ROUTE_REPLY:
+			var reply Reply
+			err := json.Unmarshal(details, &reply)
+			if err != nil {
+				return err
+			}
+
+			if !reply.OK {
+				return errors.New(reply.Err)
+			}
+
+			proto.On = nextHandleFunc
+			return proto.Reply(nextCmd, nextDetails)
+		}
+
+		return errors.New("unknow cmd: " + cmd)
+	}
+}
---
+++ protocal/route/route_test.go
@@ -0,0 +1,103 @@
+package route
+
+import (
+	"errors"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/service-exposer/exposer"
+	"github.com/service-exposer/exposer/listener"
+	"github.com/service-exposer/exposer/protocal/expose"
+	"github.com/service-exposer/exposer/protocal/keepalive"
+	"github.com/service-exposer/exposer/protocal/link"
+)
+
+func Test_route(t *testing.T) {
+	ln, dial := listener.Pipe()
+
+	go exposer.Serve(ln, func(conn net.Conn) exposer.ProtocalHandler {
+		proto := exposer.NewProtocal(conn)
+		proto.On = ServerSide()
+		return proto
+	})
+
+	func() {
+		conn, err := dial()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		cmds := make(chan string)
+
+		proto := exposer.NewProtocal(conn)
+		handlefn := keepalive.ClientSide(100 * time.Millisecond)
+		proto.On = ClientSide(func(proto *exposer.Protocal, cmd string, details []byte) error {
+			cmds <- cmd
+			return handlefn(proto, cmd, details)
+		}, keepalive.CMD_PING, nil)
+		go proto.Request(CMD_ROUTE, &RouteReq{
+			Type: KeepAlive,
+		})
+
+		cmd := <-cmds
+		if cmd != keepalive.CMD_PONG {
+			t.Fatal("expect", keepalive.CMD_PONG, "got", cmd)
+		}
+	}()
+
+	func() {
+		conn, err := dial()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		cmds := make(chan string)
+
+		proto := exposer.NewProtocal(conn)
+		handlefn := expose.ClientSide(func() (net.Conn, error) {
+			return nil, errors.New("test dial")
+		})
+		proto.On = ClientSide(func(proto *exposer.Protocal, cmd string, details []byte) error {
+			cmds <- cmd
+			return handlefn(proto, cmd, details)
+		}, expose.CMD_EXPOSE, &expose.ExposeReq{})
+
+		go proto.Request(CMD_ROUTE, &RouteReq{
+			Type: Expose,
+		})
+
+		cmd := <-cmds
+		if cmd != expose.CMD_EXPOSE_REPLY {
+			t.Fatal("expect", expose.CMD_EXPOSE_REPLY, "got", cmd)
+		}
+	}()
+
+	func() {
+		conn, err := dial()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		cmds := make(chan string)
+
+		proto := exposer.NewProtocal(conn)
+
+		ln, _ := listener.Pipe()
+		handlefn := link.ClientSide(ln)
+
+		proto.On = ClientSide(func(proto *exposer.Protocal, cmd string, details []byte) error {
+			cmds <- cmd
+			return handlefn(proto, cmd, details)
+		}, link.CMD_LINK, &link.LinkReq{})
+
+		go proto.Request(CMD_ROUTE, &RouteReq{
+			Type: Link,
+		})
+
+		cmd := <-cmds
+		if cmd != link.CMD_LINK_REPLY {
+			t.Fatal("expect", link.CMD_LINK_REPLY, "got", cmd)
+		}
+	}()
+}
