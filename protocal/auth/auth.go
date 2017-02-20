be9b25c8a6738f5bb7cd84c602fe71807f4b7574
mofon <immofon@163.com>
2017-02-20 16:55:53 +0800

    add protocal auth

---
+++ protocal/auth/auth.go
@@ -0,0 +1,110 @@
+package auth
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/service-exposer/exposer"
+	"github.com/service-exposer/exposer/protocal/route"
+)
+
+const (
+	CMD_AUTH       = "auth"
+	CMD_AUTH_REPLY = "auth:reply"
+)
+
+type Reply struct {
+	OK  bool
+	Err string
+}
+
+type AuthReq struct {
+	Key string
+}
+
+func ServerSide(authFn func(key string) (allow bool)) exposer.HandshakeHandleFunc {
+	return func(proto *exposer.Protocal, cmd string, details []byte) error {
+		switch cmd {
+		case CMD_AUTH:
+			var req AuthReq
+			err := json.Unmarshal(details, &req)
+			if err != nil {
+				return err
+			}
+
+			if !authFn(req.Key) {
+				err := errors.New("auth: forbidden key")
+				proto.Reply(CMD_AUTH_REPLY, &Reply{
+					OK:  false,
+					Err: err.Error(),
+				})
+
+				return err
+			}
+
+			err = proto.Reply(CMD_AUTH_REPLY, &Reply{
+				OK: true,
+			})
+			if err != nil {
+				return err
+			}
+
+			session := proto.Multiplex(false)
+			for {
+				conn, err := session.Accept()
+				if err != nil {
+					return err
+				}
+
+				proto_next := exposer.NewProtocal(conn)
+				proto_next.On = route.ServerSide()
+				go proto_next.Handle()
+			}
+
+			return nil
+		}
+
+		return errors.New("unknow cmd: " + cmd)
+	}
+}
+
+type NextRoute struct {
+	Req        route.RouteReq
+	HandleFunc exposer.HandshakeHandleFunc
+	Cmd        string
+	Details    interface{}
+}
+
+func ClientSide(routes <-chan NextRoute) exposer.HandshakeHandleFunc {
+	return func(proto *exposer.Protocal, cmd string, details []byte) error {
+		switch cmd {
+		case CMD_AUTH_REPLY:
+			var reply Reply
+			err := json.Unmarshal(details, &reply)
+			if err != nil {
+				return err
+			}
+
+			if !reply.OK {
+				return errors.New(reply.Err)
+			}
+
+			session := proto.Multiplex(true)
+
+			for r := range routes {
+				conn, err := session.Open()
+				if err != nil {
+					return err
+				}
+
+				proto_next := exposer.NewProtocal(conn)
+				proto_next.On = route.ClientSide(r.HandleFunc, r.Cmd, r.Details)
+				go proto_next.Request(route.CMD_ROUTE, &r.Req)
+			}
+
+			return nil
+		}
+
+		return errors.New("unknow cmd: " + cmd)
+	}
+}
---
+++ protocal/auth/auth_test.go
@@ -0,0 +1,110 @@
+package auth
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/service-exposer/exposer"
+	"github.com/service-exposer/exposer/listener"
+	"github.com/service-exposer/exposer/protocal/keepalive"
+	"github.com/service-exposer/exposer/protocal/route"
+)
+
+func Test_auth(t *testing.T) {
+	ln, dial := listener.Pipe()
+
+	authRes := make(chan bool)
+
+	go exposer.Serve(ln, func(conn net.Conn) exposer.ProtocalHandler {
+		proto := exposer.NewProtocal(conn)
+		proto.On = ServerSide(func(key string) bool {
+			auth := key == "test"
+			authRes <- auth
+			return auth
+		})
+		return proto
+	})
+
+	func() {
+		conn, err := dial()
+		if err != nil {
+			t.Fatal(err)
+		}
+		nextRoutes := make(chan NextRoute)
+
+		proto := exposer.NewProtocal(conn)
+		proto.On = ClientSide(nextRoutes)
+		go proto.Request(CMD_AUTH, &AuthReq{
+			Key: "",
+		})
+
+		res := <-authRes
+		if res != false {
+			t.Fatal("expect auth failure")
+		}
+	}()
+
+	func() {
+		conn, err := dial()
+		if err != nil {
+			t.Fatal(err)
+		}
+		nextRoutes := make(chan NextRoute, 2)
+
+		proto := exposer.NewProtocal(conn)
+		proto.On = ClientSide(nextRoutes)
+		go proto.Request(CMD_AUTH, &AuthReq{
+			Key: "test",
+		})
+
+		res := <-authRes
+		if res != true {
+			t.Fatal("expect auth correct")
+		}
+
+		keepaliveCmds_1 := make(chan string)
+		nextRoutes <- NextRoute{
+			Req: route.RouteReq{
+				Type: route.KeepAlive,
+			},
+			HandleFunc: func() exposer.HandshakeHandleFunc {
+				handlefn := keepalive.ClientSide(100 * time.Millisecond)
+
+				return func(proto *exposer.Protocal, cmd string, details []byte) error {
+					keepaliveCmds_1 <- cmd
+					return handlefn(proto, cmd, details)
+				}
+			}(),
+			Cmd: keepalive.CMD_PING,
+		}
+
+		keepaliveCmds_2 := make(chan string)
+		nextRoutes <- NextRoute{
+			Req: route.RouteReq{
+				Type: route.KeepAlive,
+			},
+			HandleFunc: func() exposer.HandshakeHandleFunc {
+				handlefn := keepalive.ClientSide(100 * time.Millisecond)
+
+				return func(proto *exposer.Protocal, cmd string, details []byte) error {
+					keepaliveCmds_2 <- cmd
+					return handlefn(proto, cmd, details)
+				}
+			}(),
+			Cmd: keepalive.CMD_PING,
+		}
+
+		cmd_1 := <-keepaliveCmds_1
+		if cmd_1 != keepalive.CMD_PONG {
+			t.Fatal("expect", keepalive.CMD_PONG, "got", cmd_1)
+		}
+
+		cmd_2 := <-keepaliveCmds_2
+		if cmd_1 != keepalive.CMD_PONG {
+			t.Fatal("expect", keepalive.CMD_PONG, "got", cmd_2)
+		}
+
+	}()
+
+}
